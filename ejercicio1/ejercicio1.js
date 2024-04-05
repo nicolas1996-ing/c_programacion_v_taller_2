98d33eb1186fe3cd3f6a2f3c0752d9b386524a96
jose nicolas aristizabal ramirez <josenicolasaristizabalramirez@gmail.com>
2024-04-04 20:14:59 -0500

    update ejercicio1

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,1 +1,15 @@
 console.log("2024-04-04");
+
+const dataFromConsole = process.argv;
+
+/*
+    npm run ejercicio1 5 10
+    dataFromConsole[0] -> node ...
+    dataFromConsole[1] -> ...ejercicio1/ejercicio1.js
+    dataFromConsole[2]
+    dataFromConsole[3]
+*/
+
+const number1 = +dataFromConsole[2];
+const number2 = +dataFromConsole[3];
+console.log(number1 + number2);
